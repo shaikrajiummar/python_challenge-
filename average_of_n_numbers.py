c003c57241c5e573007c642977dcf34fed271e09
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2026-07-02 18:43:05 +0530

    Add average_of_n_numbers.py

---
+++ average_of_n_numbers.py
@@ -0,0 +1,8 @@
+# Read the integer N
+N = int(input())
+
+# Calculate the average of the first N natural numbers
+# (Sum of first N numbers is N*(N+1)/2, so average is (N+1)/2)
+avg = (N + 1) / 2.0
+
+print(avg)
