d6c0401a26df50ad3d109efe2682f11edb0f7624
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2026-07-27 19:01:00 +0530

    Update Strings Day 3: inverted_pyramid.py with string multiplication version

--- Strings/day3/inverted_pyramid.py
+++ Strings/day3/inverted_pyramid.py
@@ -1,12 +1,9 @@
 # Read the size of the pyramid N
 n = int(input())
 
-# Loop to print the inverted star pyramid with increasing indentation
+# Loop to print the inverted star pyramid using string multiplication
 for i in range(n):
-    print(" " * (4 * i), end="")
-    for j in range(2 * (n - i) - 1):
-        if j == 2 * (n - i) - 2:
-            print("*", end="")
-        else:
-            print("* ", end="")
-    print()
+    stars = 2 * (n - i) - 1
+    space = 4 * i
+    row = " " * space + "* " * stars
+    print(row)
