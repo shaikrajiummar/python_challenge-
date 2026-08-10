351ecfcdbcfae48648429b1df90d8022740d28d8
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2026-08-10 18:28:22 +0530

    Update inverted_hollow_pyramid_numbers.py to match user implementation logic

--- Strings/day9/inverted_hollow_pyramid_numbers.py
+++ Strings/day9/inverted_hollow_pyramid_numbers.py
@@ -1,19 +1,20 @@
-# Read N (rows) and S (starting number)
+# Read size N and starting number S
 n = int(input())
 s = int(input())
 
 # Loop to print the inverted hollow pyramid pattern of numbers
-for r in range(n):
-    # Print leading spaces for alignment
-    print(" " * r, end="")
-    
-    row_elements = n - r
-    for j in range(row_elements):
-        num = s + j
-        # Print numbers on boundaries: top row, first column, or last column
-        if r == 0 or j == 0 or j == row_elements - 1:
-            print(num, end=" ")
+for i in range(n):
+    # Print leading spaces for alignment (single spaces matching NxtWave pattern)
+    print(" " * i, end="")
+
+    for j in range(i, n):
+        if i == 0:
+            print(s + j, end=" ")
+        elif j == i:
+            print(s, end=" ")
+        elif j == n - 1:
+            print(s + n - i - 1, end=" ")
         else:
-            # Print spaces matching the size of the number plus the trailing space
-            print(" " * len(str(num)) + " ", end="")
+            print("  ", end="")
+
     print()
