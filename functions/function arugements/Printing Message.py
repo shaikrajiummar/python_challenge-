b4213832b477bbbfd3a10fb62f66ae14ed6a11e8
shaikrajiummar <shaik.rajiummar.shaik@gmail.com>
2026-08-31 14:08:03 +0530

    Updated project

---
+++ functions/function arugements/Printing Message.py
@@ -0,0 +1,9 @@
+def message(arg_1, arg_2):
+    # Complete this function
+    print(arg_1 + " is " + str(arg_2) + " years old.")
+
+
+name = input()
+age = int(input())
+# Call the message function
+message(name, age)
